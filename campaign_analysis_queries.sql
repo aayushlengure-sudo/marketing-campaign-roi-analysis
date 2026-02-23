431210ebbbeeaedb60552f349a68fc00506e8e1b
aayushlengure-sudo <aayushlengure@gmail.com>
2026-02-23 19:38:55 +0530

    Update campaign_analysis_queries.sql

--- campaign_analysis_queries.sql
+++ campaign_analysis_queries.sql
@@ -1,1 +1,9 @@
+-- Calculate CTR
+SELECT campaign_name,
+       (clicks * 100.0 / impressions) AS CTR
+FROM campaigns;
 
+-- Calculate ROI
+SELECT campaign_name,
+       ((conversions * 1000 - ad_spend) / ad_spend) * 100 AS ROI
+FROM campaigns;
